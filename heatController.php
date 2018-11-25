9c48dec3122fa8a462bcd31fbe9387aac7a4829f
bladesc <bladesc@interia.pl>
2018-11-25 21:07:39 +0100

    added comments

--- heatController.php
+++ heatController.php
@@ -5,15 +5,39 @@
  * Date: 25.11.2018
  * Time: 17:40
  */
+
+
+/**
+ * 1. STARTING VARIABLES
+ */
+
+//Temperature in outside
 $tempInside = 10;
+
+//Temperature desired
 $tempDesired = 21.00;
+
+
+/**
+ * 2. INITIATION LINGUISTIC VARIABLE - DIFFERENCE, HEATING
+ */
+
+//Temperature difference
 $tempDifference = round ($tempDesired - $tempInside, 2);
+
+//Array for for linguistic variable - DIFFERANCE
 $difference = [];
+
+//Array for for linguistic variable - HEATING
 $heating = [];
+
 //LINGUISTIC VARIABLE - DIFFERENCE [v.small, small, medium, big, v.big]
-//array from -10 to 50, increments = 0.01 (changeable)
+//array from -10 to 50, increments = 0.01, [min differance = 0, max differance = 40]
 for ($i = -10; $i <= 50; $i = $i + 0.01) {
+
+    //Needed to precise couse it's bad rounding in further iterations (ex: $i = 0.0000001 instead 0.01)
     $i = round($i, 2);
+
     //VARIABLE - V.SMALL
     if ($i <= -10) {
         $difference['v.small']["$i"] = 0;
@@ -24,6 +48,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 10) {
         $difference['v.small']["$i"] = 0;
     }
+
     //VARIABLE - SMALL
     if ($i <= 0) {
         $difference['small']["$i"] = 0;
@@ -34,6 +59,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 20) {
         $difference['small']["$i"] = 0;
     }
+
     //VARIABLE - MEDIUM
     if ($i <= 10) {
         $difference['medium']["$i"] = 0;
@@ -44,6 +70,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 30) {
         $difference['medium']["$i"] = 0;
     }
+
     //VARIABLE - BIG
     if ($i <= 20) {
         $difference['big']["$i"] = 0;
@@ -54,6 +81,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 40) {
         $difference['big']["$i"] = 0;
     }
+
     //VARIABLE - V.BIG
     if ($i <= 30) {
         $difference['v.big']["$i"] = 0;
@@ -71,10 +99,11 @@ print_r($difference);
 echo "</pre>";*/
 
 //LINGUISTIC VARIABLE - HEATING [v.small, small, medium, big, v.big]
-//array from -10 to 50, increments = 0.01 (changeable)
+//array from -10 to 50, increments = 0.01, [min heating = 0, max heating = 40]
 for ($i = -10; $i <= 50; $i = $i + 0.01) {
 
     $i = round($i, 2);
+
     //VARIABLE - V.SMALL
     if ($i <= -10) {
         $heating['v.small']["$i"] = 0;
@@ -85,6 +114,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 10) {
         $heating['v.small']["$i"] = 0;
     }
+
     //VARIABLE - SMALL
     if ($i <= 0) {
         $heating['small']["$i"] = 0;
@@ -95,6 +125,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 20) {
         $heating['small']["$i"] = 0;
     }
+
     //VARIABLE - MEDIUM
     if ($i <= 10) {
         $heating['medium']["$i"] = 0;
@@ -105,6 +136,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 30) {
         $heating['medium']["$i"] = 0;
     }
+
     //VARIABLE - BIG
     if ($i <= 20) {
         $heating['big']["$i"] = 0;
@@ -115,6 +147,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     } elseif ($i >= 40) {
         $heating['big']["$i"] = 0;
     }
+
     //VARIABLE - V.BIG
     if ($i <= 30) {
         $heating['v.big']["$i"] = 0;
@@ -126,8 +159,15 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
         $heating['v.big']["$i"] = 0;
     }
 }
+
+/**
+ * 3. FUZZIFICATION
+ */
+
+//Array for all fuzzy values [v.small, small, middle, big, v.big]
 $fuzzyValues = [];
 
+//It searches value for provided difference of temperature and adds to $fuzzyArray
 foreach ($difference as $key => $value) {
     $fuzzyValues[] = [$difference[$key]["$tempDifference"], $key];
 }
@@ -137,7 +177,13 @@ print_r($fuzzyValues);
 echo "</pre>";*/
 
 
+/**
+ * 3. CONCLUSION
+ */
+
+//Array for CONCLUSION (function min($fuzzyValue, chosen heating array <- based on rules)
 $heatingAfterConc = [];
+
 //RULE IF difference = v.small THEN heating = v.big
 $min = $fuzzyValues[0][0];
 $heatingAfterConc[] = getHeatingAfeterConc($min, $heating['v.small']);
@@ -154,6 +200,8 @@ $heatingAfterConc[] = getHeatingAfeterConc($min, $heating['big']);
 $min = $fuzzyValues[4][0];
 $heatingAfterConc[] = getHeatingAfeterConc($min, $heating['v.big']);
 
+//It returns array after performing function min(fuzzy value, arr)
+//It performs function in heating variable!
 function getHeatingAfeterConc($min, array $heating): array
 {
     for ($i = -10; $i <= 50; $i = $i + 0.01) {
@@ -174,6 +222,11 @@ print_r($heatingAfterConc);
 echo "</pre>";*/
 
 
+/**
+ * 4. AGGREGATION
+ */
+
+//Array for aggregation (function max(all terms after conclusion)
 $afterAggregation = [];
 for ($i = -10; $i <= 50; $i = $i + 0.01) {
 
@@ -191,10 +244,14 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
 print_r($afterAggregation);
 echo "</pre>";*/
 
+/**
+ * 5. DEFUZZIFICATION / SHARPENING
+ */
 
-//WYOSTRZENIE
 $nominator = 0;
 $denominator = 0;
+
+//It calculates sharpen value based on array for aggregation
 for ($i = -10; $i <= 50; $i = $i + 0.01) {
 
     $i = round($i, 2);
@@ -202,5 +259,7 @@ for ($i = -10; $i <= 50; $i = $i + 0.01) {
     $nominator = round($nominator + ($i * $afterAggregation["$i"]), 2);
     $denominator = $denominator + $afterAggregation["$i"];
 }
+
 $sharpen_value = round($nominator / $denominator, 2);
+
 echo $sharpen_value;
